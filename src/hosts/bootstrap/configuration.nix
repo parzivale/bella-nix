aa989a4cc4395b6acaec3ec900fbfbf5ddfeea31
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-01 01:15:03 +0100

    stuff

--- src/hosts/bootstrap/configuration.nix
+++ src/hosts/bootstrap/configuration.nix
@@ -1,7 +1,6 @@
 {
   vars,
   pkgs,
-  inputs,
   ...
 }: let
   user = vars.username;
@@ -11,12 +10,13 @@ in {
     home.stateVersion = "25.11";
   };
 
+  home-manager.programs.home-manaegr.enable = false;
+
   users.users.nixos-anywhere = {
-    openssh.authorizedKeys.keyFiles = [./secrets/yubikey_sshkey.pub];
+    openssh.authorizedKeys.keyFiles = [../../common/secrets/yubikey_sshkey.pub];
     isNormalUser = true;
     hashedPassword = "$y$j9T$3SYXqLHQFhpwfTY8BHXmw.$cQGsYVD7CIWC22AJu1sX8qg4Po8Cyd00KzL9mAXa5F7";
     extraGroups = ["wheel"];
-    uid = 1001;
   };
 
   services.getty.autologinUser = user;
@@ -25,13 +25,12 @@ in {
     pkgs.nixos-facter
     pkgs.age
     pkgs.age-plugin-fido2-hmac
-    inputs.disktui.packages.${pkgs.stdenv.hostPlatform.system}.default
     pkgs.fzf
   ];
 
   programs.ssh.startAgent = true;
   services.avahi.enable = true;
 
-  networking.nameservers = ["1.1.1.1"];
+  networking.nameservers = ["1.1.1.1" "8.8.8.8"];
   system.stateVersion = "25.11";
 }
