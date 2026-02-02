e9f4b2dbee9aa043a1d21ad6ea50af3e41db2d87
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-02 01:10:35 +0100

    removed test data and added check line

--- template/host/configuration.nix
+++ template/host/configuration.nix
@@ -1,12 +1,15 @@
 {
   pkgs,
   vars,
+  lib,
   ...
-}: {
+}: let
+  key = builtins.readFile ./ssh_host_ed25519_key;
+in {
   system.stateVersion = "25.11";
   home-manager.users.${vars.username}.home.stateVersion = "25.11";
 
-  age.rekey.hostPubkey = builtins.readFile ./ssh_host_ed25519_key;
+  age.rekey.hostPubkey = lib.mkIf (key != "") key;
 
   programs.ssh.startAgent = true;
 }
