ccf5be4202f0e3771063b846e20d2a8c9a676e0d
hexfae <hexfae@proton.me>
2026-02-09 23:44:24 +0100

    add minecraft-server host

---
+++ src/hosts/minecraft-server/disk-configuration.nix
@@ -0,0 +1,57 @@
+{...}: {
+  disko.devices = {
+    disk = {
+      main = {
+        device = "/dev/disk/by-id/" + builtins.readFile ./boot_disk;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
+            };
+            persistent = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"]; # Override existing partition
+                subvolumes = {
+                  nix = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "discard=async"
+                      "noatime"
+                    ];
+                    mountpoint = "/nix";
+                  };
+                  persistent = {
+                    mountpoint = "/persistent";
+                    mountOptions = [
+                      "discard=async"
+                      "compress=zstd"
+                    ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "defaults"
+        "size=6G"
+        "mode=755"
+      ];
+    };
+  };
+}
