0f70d2ac39487a4bc63a8a8b64654e18a8fbfff8
hexfae <hexfae@proton.me>
2026-02-10 02:05:42 +0100

    fix the launch wezterm command on niri

--- src/common/niri.nix
+++ src/common/niri.nix
@@ -7,7 +7,7 @@
 in {
   home-manager.users.${user}.programs.niri.settings = {
     binds = with config.lib.niri.actions; {
-      "Mod+Return".action.spawn = ["wezterm" "+new-window"];
+      "Mod+Return".action.spawn = "wezterm";
     };
   };
 }
