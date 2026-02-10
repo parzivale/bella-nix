d53b45f172a5ed45aa3bfc8b45f365c062b43d50
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-10 01:32:02 +0100

    added true

--- src/common/networking.nix
+++ src/common/networking.nix
@@ -1,7 +1,7 @@
 # Common networking options (no where better to put these)
 {lib, ...}: {
   # Needed for the onboarding script
-  programs.ssh.startAgent = lib.mkOverride 2000;
+  programs.ssh.startAgent = lib.mkOverride 2000 true;
 
   # Not needed but makes tailscale magic dns better
   services.resolved.enable = lib.mkDefault true;
