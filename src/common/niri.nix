a7657d622bb116cd05f22879d01069a87c528cf3
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-10 01:11:03 +0100

    fuck im bad at home mananger

--- src/common/niri.nix
+++ src/common/niri.nix
@@ -1,5 +1,11 @@
-{config, ...}: {
-  programs.niri.settings = {
+{
+  config,
+  vars,
+  ...
+}: let
+  user = vars.username;
+in {
+  home-manager.users.${user}.programs.niri.settings = {
     binds = with config.lib.niri.actions; {
       "Mod+Return".action.spawn = ["wezterm" "+new-window"];
     };
