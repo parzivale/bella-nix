a6e86582cbd5486799660da9623b9683852a741d
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-12 03:17:34 +0100

    typo

--- src/common/programs/helix.nix
+++ src/common/programs/helix.nix
@@ -57,7 +57,7 @@ in {
         }
         {
           name = "markdown";
-          soft-wrap.enable = "true";
+          soft-wrap.enable = true;
           text-width = 80;
           soft-wrap.wrap-at-text-width = true;
           language-servers = ["marksman" "harper-ls"];
