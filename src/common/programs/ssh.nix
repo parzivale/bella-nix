0aeebd20b30ce8460553690a8cdbca3d71321bbb
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-12 04:14:47 +0100

    stuff

--- src/common/programs/ssh.nix
+++ src/common/programs/ssh.nix
@@ -13,6 +13,19 @@ in {
         identityFile = config.age.secrets.github-key.path;
         identitiesOnly = true;
       };
+
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
     };
   };
 }
