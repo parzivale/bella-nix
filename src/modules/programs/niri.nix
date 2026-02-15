47c52e57b47c53a0ae82be43a78b295cf8123abf
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-15 23:28:42 +0100

    added niri keybinds

--- src/modules/programs/niri.nix
+++ src/modules/programs/niri.nix
@@ -22,6 +22,26 @@
     home-manager.users.${user}.programs.niri.settings = {
       binds = with config.lib.niri.actions; {
         "Mod+Return".action.spawn = "wezterm";
+        "Print".action = screenshot;
+
+        "Mod+H".action = focus-column-or-monitor-left;
+        "Mod+L".action = focus-column-or-monitor-right;
+        "Mod+J".action = focus-window-or-workspace-down;
+        "Mod+K".action = focus-window-or-workspace-up;
+
+        "Mod+Shift+H".action = move-column-left-or-to-monitor-left;
+        "Mod+Shift+L".action = move-column-right-or-to-monitor-right;
+        "Mod+Shift+J".action = move-window-down-or-to-workspace-down;
+        "Mod+Shift+K".action = move-window-up-or-to-workspace-up;
+
+        "Mod+F".action = maximize-column;
+        "Mod+Shift+F".action = fullscreen-window;
+        "Mod+Q".action = close-window;
+        "Mod+R".action = switch-preset-column-width;
+        "Mod+Tab".action = toggle-overview;
+        "Mod+T".action = toggle-column-tabbed-display;
+
+        "Mod+Shift+E".action.quit.skip-confirmation = true;
       };
 
       input = {
