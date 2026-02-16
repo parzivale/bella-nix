edb2cf828edc08a4fa9f88d6a56db17cf3cd5426
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-16 21:05:47 +0100

    whoops

--- src/modules/programs/yazi.nix
+++ src/modules/programs/yazi.nix
@@ -2,7 +2,7 @@
   flake.modules.nixos.yazi = {config, ...}: let
     user = config.systemConstants.username;
   in {
-    hoe-manager.users.${user} = {
+    home-manager.users.${user} = {
       programs.yazi = {
         enable = true;
         enableNushellIntegration = true;
