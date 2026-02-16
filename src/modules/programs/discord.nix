58c56375c94906240109f60de84effa2c7200936
parzivale <40600864+parzivale@users.noreply.github.com>
2026-02-16 23:23:13 +0100

    fixed discord hopefully

--- src/modules/programs/discord.nix
+++ src/modules/programs/discord.nix
@@ -1,10 +1,26 @@
 {
-  flake.modules.nixos.discord = {config, ...}: let
+  flake.modules.nixos.discord = {
+    config,
+    pkgs,
+    ...
+  }: let
     user = config.systemConstants.username;
   in {
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-gnome
+      ];
+      config = {
+        common.default = ["gnome"];
+      };
+    };
     home-manager.users.${user} = {
       programs.discord = {
         enable = true;
+        package = pkgs.discord-canary;
       };
     };
   };
--- src/modules/programs/niri.nix
+++ src/modules/programs/niri.nix
@@ -31,17 +31,6 @@
       lib,
       ...
     }: {
-      xdg.portal = {
-        enable = true;
-        xdgOpenUsePortal = true;
-        extraPortals = [
-          pkgs.xdg-desktop-portal-gtk
-          pkgs.xdg-desktop-portal-gnome
-        ];
-        config = {
-          common.default = ["gnome"];
-        };
-      };
       programs.niri = {
         package = pkgs.niri-unstable;
         settings = {
