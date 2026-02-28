0f64424007f44e1a7d864313aedda19e73c6edb4
parzivale <zeus@theolivers.org>
2026-02-28 20:42:37 +0100

    tweak gtnh config

--- src/modules/services/gtnh.nix
+++ src/modules/services/gtnh.nix
@@ -1,7 +1,18 @@
 {inputs, ...}: {
   flake.modules.nixos.gtnh = {
     imports = with inputs.gtnh-nix; [nixosModules.gtnh nixosModules."2.8.4"];
-    programs.gtnh.enable = true;
+    programs.gtnh = {
+      enable = true;
+      minecraft = {
+        instance-options = {
+          jvmMaxAllocation = "15G";
+          jvmInitialAllocation = "6G";
+        };
+        server-properties = {
+          max-tick-time = -1;
+        };
+      };
+    };
     preservation = {
       preserveAt."/persistent".directories = ["/var/lib/gtnh"];
     };
