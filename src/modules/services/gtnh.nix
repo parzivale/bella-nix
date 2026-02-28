3ca6ae8b268de51170aada0565349775fd872c61
parzivale <zeus@theolivers.org>
2026-02-28 20:20:51 +0100

    actually enable gtnh

--- src/modules/services/gtnh.nix
+++ src/modules/services/gtnh.nix
@@ -1,6 +1,7 @@
 {inputs, ...}: {
   flake.modules.nixos.gtnh = {
     imports = with inputs.gtnh-nix; [nixosModules.gtnh nixosModules."2.8.4"];
+    programs.gtnh.enable = true;
     preservation = {
       preserveAt."/persistent".directories = ["/var/lib/gtnh"];
     };
