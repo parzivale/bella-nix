7122396fc0b7a7a7c7b33c1da69a6b6347245403
parzivale <zeus@theolivers.org>
2026-02-28 20:11:54 +0100

    fuck me typos

--- src/hosts/minecraft-server/configuration.nix
+++ src/hosts/minecraft-server/configuration.nix
@@ -14,6 +14,7 @@ in {
     deployable
     cli
     localization
+    gtnh
   ];
 
   system.stateVersion = "25.11";
--- src/modules/services/gnth.nix
+++ src/modules/services/gnth.nix
@@ -1,5 +1,5 @@
 {inputs, ...}: {
-  flake.modules.nixos.gnth = {config, ...}: {
+  flake.modules.nixos.gtnh = {
     imports = with inputs.gnth-nix.nixosModules; [gtnh "2.8.4"];
     preservation = {
       preserveAt."persistent".directories = ["/var/lib/gtnh"];
