c61d28dacea5f0daa560a356eec84ec4667a8df2
parzivale <zeus@theolivers.org>
2026-03-02 18:19:16 +0100

    addded steam persistance

--- src/modules/programs/steam.nix
+++ src/modules/programs/steam.nix
@@ -1,9 +1,23 @@
 {
-  flake.modules.nixos.steam = {
+  flake.modules.nixos.steam = {config, ...}: let
+    user = config.systemConstants.username;
+  in {
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports for Source Dedicated Server hosting
+    };
+    preservation.preserveAt."/persistent".users.${user} = {
+      directories = [
+        {
+          directory = ".steam";
+          mode = "0755";
+        }
+        {
+          directory = ".local/share/Steam";
+          mode = "0755";
+        }
+      ];
     };
   };
 }
