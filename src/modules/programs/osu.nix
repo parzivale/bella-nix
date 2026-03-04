66e3582659ad515640598d1ea26d7093cc588379
parzivale <zeus@theolivers.org>
2026-03-04 18:17:14 +0100

    added persistence to osu

--- src/modules/programs/osu.nix
+++ src/modules/programs/osu.nix
@@ -7,5 +7,14 @@
     user = config.systemConstants.username;
   in {
     home-manager.users.${user}.home.packages = [pkgs.osu-lazer-bin];
+
+    preservation.preserveAt."/persistent".users.${user} = {
+      directories = [
+        {
+          directory = ".local/share/osu";
+          mode = "0755";
+        }
+      ];
+    };
   };
 }
