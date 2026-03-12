73bcbc65b7e838bd1d382add414205d9284f29a9
parzivale <zeus@theolivers.org>
2026-03-13 00:59:15 +0100

    added persistance to table config

--- src/modules/programs/tablet.nix
+++ src/modules/programs/tablet.nix
@@ -1,8 +1,19 @@
 {
-  flake.modules.nixos.tablet = {
+  flake.modules.nixos.tablet = {config, ...}: let
+    user = config.systemConstants.username;
+  in {
     hardware.opentabletdriver = {
       enable = true;
       daemon.enable = true;
+    };
+
+    preservation.preserveAt."/persistent".users.${user} = {
+      directories = [
+        {
+          directory = ".config/OpenTableDriver";
+          mode = "0755";
+        }
+      ];
     };
   };
 }
