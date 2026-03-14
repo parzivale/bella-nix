601467a49a80b8ae38f754e8f9b7920eb58da3fe
parzivale <zeus@theolivers.org>
2026-03-14 19:36:13 +0100

    added stuff

--- src/modules/programs/prismlauncher.nix
+++ src/modules/programs/prismlauncher.nix
@@ -6,6 +6,12 @@
   }: let
     user = config.systemConstants.username;
   in {
+    # nix-ld provides /lib64/ld-linux-x86-64.so.2 for MCEF's libcef.so
+    programs.nix-ld = {
+      enable = true;
+      libraries = with pkgs; [nss nspr libgbm];
+    };
+
     home-manager.users.${user}.home.packages = [
       (pkgs.prismlauncher.override {
         additionalLibs = with pkgs; [nss nspr libgbm];
