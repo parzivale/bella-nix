c832887bcc172c60e588944e99ef9d8f22d8612d
parzivale <zeus@theolivers.org>
2026-03-14 19:10:18 +0100

    goddamn CEF needs a ton of deps

--- src/modules/programs/prismlauncher.nix
+++ src/modules/programs/prismlauncher.nix
@@ -7,7 +7,9 @@
     user = config.systemConstants.username;
   in {
     home-manager.users.${user}.home.packages = [
-      (pkgs.prismlauncher.override {additionalLibs = with pkgs; [nss nspr libgbm];})
+      (pkgs.prismlauncher.override {
+        additionalLibs = with pkgs; [nss nspr libgbm libdrm expat libxkbcommon dbus alsa-lib];
+      })
     ];
 
     preservation.preserveAt."/persistent".users.${user}.directories = [
