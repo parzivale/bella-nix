138fe9f81d6c3fa8a62afc07d94f243ef88a2df9
parzivale <zeus@theolivers.org>
2026-03-14 18:53:40 +0100

    added libgbm

--- src/modules/programs/prismlauncher.nix
+++ src/modules/programs/prismlauncher.nix
@@ -7,7 +7,7 @@
     user = config.systemConstants.username;
   in {
     home-manager.users.${user}.home.packages = [
-      (pkgs.prismlauncher.override {additionalLibs = with pkgs; [nss nspr mesa];})
+      (pkgs.prismlauncher.override {additionalLibs = with pkgs; [nss nspr libgbm];})
     ];
 
     preservation.preserveAt."/persistent".users.${user}.directories = [
