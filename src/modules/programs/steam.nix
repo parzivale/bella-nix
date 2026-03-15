06874e1844f528acc654ebc142a90166cb04075f
parzivale <zeus@theolivers.org>
2026-03-15 23:31:46 +0100

    more steam flags

--- src/modules/programs/steam.nix
+++ src/modules/programs/steam.nix
@@ -9,7 +9,11 @@
     programs.steam = {
       enable = true;
       package = pkgs.steam.override {
-        extraArgs = "-dev -console -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -cef-disable-d3d11 -cef-disable-sandbox -disable-winh264 -cef-force-32bit -no-cef-sandbox -vrdisable -cef-disable-breakpad";
+        extraEnv = {
+          STEAM_FRAME_FORCE_CLOSE = "1";
+          STEAM_DISABLE_BROWSER_COMPOSITOR = "1";
+        };
+        extraArgs = "-no-browser -dev -console -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -cef-disable-d3d11 -cef-disable-sandbox -disable-winh264 -cef-force-32bit -no-cef-sandbox -vrdisable -cef-disable-breakpad";
       };
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports for Source Dedicated Server hosting
