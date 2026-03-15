88865d984dfd589795d920a6603daf5ddb54fcc3
parzivale <zeus@theolivers.org>
2026-03-15 23:41:57 +0100

    reduced options and forecd minigames list

--- src/modules/programs/steam.nix
+++ src/modules/programs/steam.nix
@@ -13,7 +13,7 @@
           STEAM_FRAME_FORCE_CLOSE = "1";
           STEAM_DISABLE_BROWSER_COMPOSITOR = "1";
         };
-        extraArgs = "-no-browser -dev -console -nofriendsui -no-dwrite -nointro -nobigpicture -nofasthtml -nocrashmonitor -noshaders -no-shared-textures -disablehighdpi -cef-single-process -cef-in-process-gpu -single_core -cef-disable-d3d11 -cef-disable-sandbox -disable-winh264 -cef-force-32bit -no-cef-sandbox -vrdisable -cef-disable-breakpad";
+        extraArgs = "-no-browser -cef-single-process -cef-in-process-gpu -no-cef-sandbox -start steam://open/minigameslist";
       };
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports for Source Dedicated Server hosting
