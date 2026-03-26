475af1c26b7e70b35f0814c9c3c566a331033228
parzivale <zeus@theolivers.org>
2026-03-26 01:03:53 +0100

    ah myb i disabled managing my network with iwd

--- src/modules/services/iwd.nix
+++ src/modules/services/iwd.nix
@@ -1,6 +1,9 @@
 {...}: {
   flake.modules.nixos.iwd = {
-    networking.wireless.iwd.enable = true;
+    networking.wireless.iwd = {
+      enable = true;
+      settings.General.EnableNetworkConfiguration = true;
+    };
 
     preservation.preserveAt."/persistent".directories = [
       "/var/lib/iwd"
