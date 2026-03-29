16db1c2a462080e355b3ef83cb78280abd581dec
parzivale <zeus@theolivers.org>
2026-03-29 19:03:33 +0200

    added more grub config

--- src/hosts/embla/hardware-configuration.nix
+++ src/hosts/embla/hardware-configuration.nix
@@ -4,6 +4,13 @@
   pkgs,
   ...
 }: {
+  boot = {
+    loader.grub = {
+      enable = true;
+      devices = "/dev/disk/by-id/" + builtins.readFile ./boot_disk;
+    };
+  };
+
   hardware.enableAllFirmware = true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
