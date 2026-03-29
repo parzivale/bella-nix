f3236314e7a658ae5c6bfb246596baeecb81fa8e
parzivale <zeus@theolivers.org>
2026-03-29 19:58:49 +0200

    maybe claude is right?

--- src/hosts/embla/hardware-configuration.nix
+++ src/hosts/embla/hardware-configuration.nix
@@ -1,16 +1,7 @@
 {inputs}: {
-  config,
   lib,
-  pkgs,
   ...
 }: {
-  boot = {
-    loader.grub = {
-      enable = true;
-      devices = [("/dev/disk/by-id/" + builtins.readFile ./boot_disk)];
-    };
-  };
-
   hardware.enableAllFirmware = true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
