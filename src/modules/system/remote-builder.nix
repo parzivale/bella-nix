e0ad2143d0b63e0ee27a5a989c1f053fcc4771e8
parzivale <zeus@theolivers.org>
2026-03-30 00:03:33 +0200

    give the no shell user a shell

--- src/modules/system/remote-builder.nix
+++ src/modules/system/remote-builder.nix
@@ -1,9 +1,10 @@
 {...}: {
-  flake.modules.nixos.remote-builder = {...}: {
+  flake.modules.nixos.remote-builder = {pkgs, ...}: {
     users.groups.nix-build = {};
     users.users.nix-builder = {
       isSystemUser = true;
       group = "nix-build";
+      shell = pkgs.bash;
       openssh.authorizedKeys.keyFiles = [../../secrets/nix-builder/nix-builder-key.pub];
     };
 
