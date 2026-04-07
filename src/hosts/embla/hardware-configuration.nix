cff8f4906385aa25f5d27f58d79e561a9a94c607
parzivale <zeus@theolivers.org>
2026-04-07 02:49:38 +0200

    fixed embla

--- src/hosts/embla/hardware-configuration.nix
+++ src/hosts/embla/hardware-configuration.nix
@@ -1,7 +1,30 @@
-{inputs}: {
-  lib,
-  ...
-}: {
+{inputs}: {lib, ...}: {
+  networking = {
+    useDHCP = false;
+    interfaces.enp3s0 = {
+      ipv4.addresses = [
+        {
+          address = "130.240.204.12";
+          prefixLength = 24;
+        }
+      ];
+      ipv6.addresses = [
+        {
+          address = "2001:6b0:10:9829:204:12";
+          prefixLength = 64;
+        }
+      ];
+    };
+    defaultGateway = {
+      address = "130.240.204.1";
+      interface = "enp3s0";
+    };
+    defaultGateway6 = {
+      address = "2001:6b0:10:9829::1";
+      interface = "enp3s0";
+    };
+  };
+
   hardware.enableAllFirmware = true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
